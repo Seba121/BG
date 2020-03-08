f3cb739cee60b35afcd74edaacb71d88bd88dbe1
Sebastian Karolczak <sebastian2827@outlook.com>
2020-03-08 13:46:10 +0100

    Button selec picture

--- Bgworker/Bgworker/Form1.cs
+++ Bgworker/Bgworker/Form1.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Bgworker
 {
     public partial class Form1 : Form
     {
+        Bitmap obrazek;
+        int r = 0, g = 0, b = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            pictureBox1.Image = new Bitmap(openFileDialog1.FileName);
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
